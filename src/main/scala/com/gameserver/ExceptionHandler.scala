d4b633f9bbd2835b4226b40b8f82baed0d8191e2
Devendra <devend@cumulusnetworks.com>
2022-08-07 22:55:40 +0530

    Corrections

--- src/main/scala/com/gameserver/ExceptionHandler.scala
+++ src/main/scala/com/gameserver/ExceptionHandler.scala
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2018-2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- * LicenseRef-NvidiaProprietary
- *
- * NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
- * property and proprietary rights in and to this material, related
- * documentation and any modifications thereto. Any use, reproduction,
- * disclosure or distribution of this material and related documentation
- * without an express license agreement from NVIDIA CORPORATION or
- * its affiliates is strictly proprietaryhibited.prohibited
- */
 package com.gameserver
 
 import akka.http.scaladsl.model.StatusCodes
